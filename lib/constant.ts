a41cef807283426f16a3c050377d3d8ffc41c233
Empre12 <proyectosdejai18@gmail.com>
2026-05-30 14:49:03 +0200

    initial commit

---
+++ lib/constant.ts
@@ -0,0 +1,7 @@
+export const SITE_URL = "https://kapve.com";
+export const SITE_NAME = "UnitConverterLite";
+export const SITE_TITLE = "Unit Converter Lite - Compute Unit Conversions Instantly.";
+export const SITE_DESCRIPTION =
+	"Free online unit converter. Compute unit conversions, currencies and more instantly. No registration required.";
+export const SITE_AUTHOR = "Deelzo";
+export const SITE_EMAIL = "amitkumarteam90@gmail.com";
