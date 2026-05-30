bb9f523b0127e6554bbbf63b892a2d7a9c44c145
Empre12 <proyectosdejai18@gmail.com>
2026-05-30 15:07:16 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,10 +72,10 @@ export default function RootLayout({
 					}}
 				/>
 				{/* Google Search Console */}
-				<meta name="google-site-verification" content="TxmexGzxRMEa83YuMpgP46SvZ4Bxo3KsWHADvOQ387Y" />
+				<meta name="google-site-verification" content="xxxxxxxxxxxxxxxxxxx" />
 				<Script
 					strategy='afterInteractive'
-					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3612241166480319' // Replace with your AdSense ID
+					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2780507259331537' // Replace with your AdSense ID
 					crossOrigin='anonymous'
 				/>
       </head>
