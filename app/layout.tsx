b301da0a7680b616a64440697c002c20d626f57d
Empre12 <proyectosdejai18@gmail.com>
2026-05-30 15:50:42 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,10 +75,12 @@ export default function RootLayout({
 				<meta name="google-site-verification" content="xxxxxxxxxxxxxxxxxxx" />
 				<Script
 					strategy='afterInteractive'
-					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2780507259331537' // google.com, pub-2780507259331537, DIRECT, f08c47fec0942fa0
+					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2780507259331537' // 
 					crossOrigin='anonymous'
 				/>
       </head>
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2780507259331537"
+     crossorigin="anonymous"></script>
       <body className="font-sans antialiased bg-background text-foreground" suppressHydrationWarning>
         <Header />
         {children}
