7685b58d33468c12ba6885c91ba83ecb80eef682
Empre12 <proyectosdejai18@gmail.com>
2026-05-30 14:56:12 +0200

    Update constant.ts

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,7 +1,7 @@
-export const SITE_URL = "https://kapve.com";
-export const SITE_NAME = "UnitConverterLite";
-export const SITE_TITLE = "Unit Converter Lite - Compute Unit Conversions Instantly.";
+export const SITE_URL = "https://financialzoneall.com";
+export const SITE_NAME = "FinancialZoneAll";
+export const SITE_TITLE = "Financial Zone All - Compute Unit Conversions Instantly.";
 export const SITE_DESCRIPTION =
 	"Free online unit converter. Compute unit conversions, currencies and more instantly. No registration required.";
-export const SITE_AUTHOR = "Deelzo";
-export const SITE_EMAIL = "amitkumarteam90@gmail.com";
+export const SITE_AUTHOR = "FinancialZoneAll";
+export const SITE_EMAIL = "financialzoneall@gmail.com";
